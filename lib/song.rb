9bb5ac43d8f6565c3abef9478246b3e349edb61e
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-24 18:01:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -58,14 +58,8 @@ class Song
     song
   end 
   
-  def self.create_from_filename
-      data = song_name.split(" - ")
-    artist_name = data[0]
-    song_name= data[1].chomp!(".mp3")
-    song = self.new 
-    song.name = song_name
-    song.artist_name = artist_name
-    song.save
-    song
+  def self.create_from_filename(song_name)
+     song = new_from_filename(song_name)
+     song.save
   end 
 end
